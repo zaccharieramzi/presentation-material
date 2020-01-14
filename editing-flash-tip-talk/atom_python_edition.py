42c2ebe7e690983f239aca94b407870acf614f33
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-01-14 13:12:02 +0100

    added the atom edition file for the flash tip talk

---
+++ editing-flash-tip-talk/atom_python_edition.py
@@ -0,0 +1,13 @@
+""""My example module"""
+# my typical multi-cursor creation/use
+class MyExampleClass:
+    # We have a class which contains a lot of private variables
+    def __init__(self, first_param, second_param, third_param, fourth_param, fifth_param, sixth_param, seventh_param):
+        # we want to initialize all these variables with input given by the user
+        self.first_param = first_param
+        self.second_param = second_param
+        self.third_param = third_param
+        self.fourth_param = fourth_param
+        self.fifth_param = fifth_param
+        self.sixth_param = sixth_param
+        self.seventh_param = seventh_param
